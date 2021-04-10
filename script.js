44c5031fa54aec3b5a2518dcab343eab68bb46fc
Nadia <nadiyaklokova@gmail.com>
2021-04-10 11:48:48 +0200

    Coming Soon Movies

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ function cambiar() {
     document.body.style.backgroundImage = 'url(./img/pared1.jpg)';
   }
 
+const getComingMovies = async() =>{
+    try {
+        const res= await axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=b5138e06a3a9125b8c326498bbeae997&language=es-ES');
+        const movies = res.data.results;
+        renderMovies(movies)   
+    } catch (error) {
+        console.error(error);
+    }
+   
+}
 
+  
 const renderMovies = movies => {
+    document.querySelector('main.movies').innerHTML =''
     for (const movie of movies){
         document.querySelector('main.movies').innerHTML+=`
         <div class="movie">
