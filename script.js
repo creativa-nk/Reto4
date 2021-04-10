4e03fb05abec12f332fa12a45dec3f59bbc658bf
Nadia <nadiyaklokova@gmail.com>
2021-04-10 13:20:18 +0200

    movie dateils

--- script.js
+++ script.js
@@ -2,6 +2,36 @@ function cambiar() {
     document.body.style.backgroundImage = 'url(./img/pared1.jpg)';
   }
 
+  const getMovieHtml = movie =>{
+      return `
+      <div class="movie" onclick='getMovieDetailed(${movie.id})'>
+          <div class='titulo'>
+               <h2>${movie.title}</h2>
+          <div>
+          <div class='imgMovie'>  
+                <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="imagen de la pelicula">
+          <div>
+      </div>
+      `
+  }
+
+  const getMovieDetailedHtml = movie =>{
+    return `
+    <div class="movie" onclick='getMovieDetailed(${movie.id})'>
+        <div class='titulo'>
+             <h2>${movie.title}</h2>
+        <div>
+        <div class='imgMovie'>  
+              <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="imagen de la pelicula">
+        <div>
+        <div class='movieDetails'
+            <span>Popularidad:${movie.popularity}</span>
+            <p>${movie.overview}</p>
+    </div>
+    `
+}
+
+
 const getComingMovies = async() =>{
     try {
         const res= await axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=b5138e06a3a9125b8c326498bbeae997&language=es-ES');
@@ -13,22 +43,20 @@ const getComingMovies = async() =>{
    
 }
 
-  
 const renderMovies = movies => {
     document.querySelector('main.movies').innerHTML =''
     for (const movie of movies){
-        document.querySelector('main.movies').innerHTML+=`
-        <div class="movie">
-            <div class='titulo'>
-                 <h2>${movie.title}</h2>
-            <div>
-            <div class='imgMovie'>  
-                  <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="">
-            <div>
-        </div>
-        `
-    }
-    
+        document.querySelector('main.movies').innerHTML += getMovieHtml(movie)
+    }   
+}
+
+const getMovieDetailed = movie_id =>{
+    axios.get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=b5138e06a3a9125b8c326498bbeae997&language=es-ES`)
+    .then(res=>{
+        const movie = res.data;
+        document.querySelector('main.movies').innerHTML = getMovieDetailedHtml(movie)
+    })
+    .catch(console.error) //es lo mismo que escribir  error=>console.error(error)
 }
 
 const getPopularMovies = () =>{
